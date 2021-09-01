880b3ea765089752e4fa3be80d82c109ad0ad486
Kyle Riley <kmr145@miami.edu>
2021-09-01 11:13:58 -0400

    updated models and main.py

--- app/app/models.py
+++ app/app/models.py
@@ -1,18 +1,50 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
 
 
-class DevResource(db.Model):
-    topic = db.Column(db.String, primary_key=True)
-    description = db.Column(db.String)
-    url_link = db.Column(db.String)
+class Linux(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    url_link = db.Column(db.String) #should this be a list?
 
-    def __init__(self, topic, description, url_link):
-        self.topic=topic
-        self.description=description
+    def __init__(self, id, url_link):
+        self.id=id
         self.url_link=url_link
 
     def __repr__(self): #prints out representation of object, 
-        return f'<topic-description-url_link : {self.topic}-{self.description}-{self.url_link}'
+        return f'<topic-description-url_link : {self.id}-{self.url_link}'
+
+class Python(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    url_link = db.Column(db.String) #should this be a list?
+
+    def __init__(self, id, url_link):
+        self.id=id
+        self.url_link=url_link
+
+    def __repr__(self): #prints out representation of object, 
+        return f'<topic-description-url_link : {self.id}-{self.url_link}'
+
+class Docker(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    url_link = db.Column(db.String) #should this be a list?
+
+    def __init__(self, id, url_link):
+        self.id=id
+        self.url_link=url_link
+
+    def __repr__(self): #prints out representation of object, 
+        return f'<topic-description-url_link : {self.id}-{self.url_link}'
+
+class AWS(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    url_link = db.Column(db.String) #should this be a list?
+
+    def __init__(self, id, url_link):
+        self.id=id
+        self.url_link=url_link
+
+    def __repr__(self): #prints out representation of object, 
+        return f'<topic-description-url_link : {self.id}-{self.url_link}'
+
+
