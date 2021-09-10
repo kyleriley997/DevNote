04b4a0d42613bd502a16fac0415b325381dcc89f
Kyle Riley <kmr145@miami.edu>
2021-09-10 14:28:22 -0400

    changed checkHealth function name to status

--- app/app/main.py
+++ app/app/main.py
@@ -18,7 +18,7 @@ red = redis.Redis(host='redis', port=6379, db=0) #redis port/default port
 client = boto3.client('elasticbeanstalk', region_name='us-west-2')
 
 @app.route("/status")
-def checkHealth():
+def status():
     response = client.describe_environments(
         ApplicationName='DevNote',
         EnvironmentNames=['DevNote-env-1']
